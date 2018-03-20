b31d426c6ca8e3365cce70b0578a050ecf60bd2a
Davor Koret <dkoret318@gmail.com>
2018-03-19 21:40:49 -0700

    Update parse_function.php

--- parse_function.php
+++ parse_function.php
@@ -1,25 +1,32 @@
 <?php
 
-// $url = 'http:// url of the page we want the content from';
+// website we want to take the content from
+include('simple_html_dom.php');
 
-// using file_get_contents function we get the specific content from the page
-$content = file_get_contents($url);
-echo $content;
-// #output# "write the content we want from the webpage"
+$html = file_get_html('https://google.co.in/');
+$count=0;
 
-// using file function 
-// read line by line in array
-$content = file($url);
-print_r($content);
+// search for keywords from the website we choose
+$searchfor1 =array("PHP","MySql","HTML","CSS","Javascript","C++","Java","C","Python");
+$contents=file_get_html('https://www.indeed.co.in/jobs?q=web%20developer&l=Bengaluru%2C%20Karnataka&vjk=c832f5eaa2bf8ee5/')->plaintext;
+$size= str_word_count($contents);
 
-// #output# Array (0] => ﻿"write the content we want from the webpage")
+echo " ";
 
-// using cURL
-$ch = curl_init($url);  
-curl_setopt($ch, CURLOPT_HEADER, 0);  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
-$content = curl_exec($ch);
-echo $content;
-//#output# "write the content we want from the webpage"
+// search for function
+foreach($searchfor1 as $searchfor) 
+{
+  $contents=file_get_html('https://www.indeed.co.in/jobs?q=web%20developer&l=Bengaluru%2C%20Karnataka&vjk=c832f5eaa2bf8ee5/')->plaintext;
+  $pattern = preg_quote($searchfor, '/');
+  $count= substr_count($contents,$pattern);
+  $per=($count/$size)*100;
+
+  echo "This Job is well suited for ".$searchfor;
+  echo " ";
+  echo $per*100;
+  echo "%";
+  echo "<br>";
+  echo " ";
+}
 
 ?>
